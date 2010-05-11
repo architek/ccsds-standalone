9a19b44ad5934deea67ccb62fa178e01e24575d6
Laurent Kislaire <teebeenator@googlemail.com>
2010-05-11 21:32:14 +0200

    Add small TC handling

---
+++ Data-ParseBinary-Network-Ccsds/lib/Data/ParseBinary/Network/Ccsds/TC/SourcePacket.pm
@@ -0,0 +1,155 @@
+package Data::ParseBinary::Network::Ccsds::TC::SourcePacket;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Data::ParseBinary::Network::Ccsds::TC::SourcePacket - The great new Data::ParseBinary::Network::Ccsds::TC::SourcePacket!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '1.02';
+
+use Data::ParseBinary;
+
+use Data::ParseBinary::Network::Ccsds::Common;
+
+my $Apid = BitStruct('Apid',
+  $Pid,
+  Nibble('Pcat')
+);
+
+my $TCSourceSecondaryHeader = Struct('TCSourceSecondaryHeader',
+  BitStruct('SecHeadFirstField',
+    BitField('Spare1',1),
+    BitField('PUS Version Number',3),
+    Nibble('Spare2')
+  ),
+  UBInt8('Service Type'),
+  UBInt8('Service Subtype'),
+  UBInt8('Destination Id'),
+  $Sat_Time,
+);
+
+
+our $tcsourcepacket= Struct('TC Source Packet',
+  Struct('Packet Header',
+        BitStruct('Packet Id',
+          BitField('Version Number',3),
+       	  BitField('Type',1),
+          Flag('DFH Flag'),
+          $Apid
+        ),
+        BitStruct('Packet Sequence Control',
+          BitField('Segmentation Flags',2),
+          BitField('Source Seq Count',14),
+          UBInt16('Packet Length'),
+          Value('Source Data Length', sub { $_->ctx->{'Packet Length'} +1 -2 - 10*$_->ctx(1)->{'Packet Id'}->{'DFH Flag'} } ),
+        )
+    ),
+
+    UBInt16('Packet Error Control'),
+   )
+);
+
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT = qw($tcsourcepacket);
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Data::ParseBinary::Network::Ccsds::TC::SourcePacket;
+
+    my $foo = Data::ParseBinary::Network::Ccsds::TC::SourcePacket->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Laurent KISLAIRE, C<< <teebeenator at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-data-parsebinary-network-ccsds at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Data-ParseBinary-Network-Ccsds>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Data::ParseBinary::Network::Ccsds::TC::SourcePacket
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Data-ParseBinary-Network-Ccsds>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Data-ParseBinary-Network-Ccsds>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Data-ParseBinary-Network-Ccsds>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Data-ParseBinary-Network-Ccsds/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2010 Laurent KISLAIRE.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of Data::ParseBinary::Network::Ccsds::TC::SourcePacket

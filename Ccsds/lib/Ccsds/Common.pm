6f553a81ceb4fa41ee5a51c5a0e5bdc775b3cc3f
Laurent KISLAIRE <teebeenator@googlemail.com>
2011-07-10 19:38:46 +0000

    Use CUC

--- Ccsds/lib/Ccsds/Common.pm
+++ Ccsds/lib/Ccsds/Common.pm
@@ -15,19 +15,16 @@ use Data::ParseBinary;
 
 our $Sat_Time = Struct( 'Sat_Time',
     UBInt32('Seconds'),
+    UBInt8('SubSecondsH'),
     UBInt16('SubSeconds'),
     Value(
-        'OBT', sub { $_->ctx->{'Seconds'} + $_->ctx->{'SubSeconds'} / 65535 }
+        'OBT', sub { $_->ctx->{'Seconds'} + ($_->ctx->{'SubSecondsH'}<<16 +$_->ctx->{'SubSeconds'}) / 524280 }
     )
 );
 
 our $Pid = Enum(
     BitField( 'PID', 7 ),
       TIME        => 0x0,
-      SYS         => 0x10,
-      AOC         => 0x11,
-      PF          => 0x12,
-      PL          => 0x13,
       _default_   => $DefaultPass
 );
 

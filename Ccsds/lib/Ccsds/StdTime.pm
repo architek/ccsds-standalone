a8ac077ea809fd1321745471ca23f777a5f9674a
Laurent Kislaire <teebeenator@googlemail.com>
2011-08-12 21:44:02 +0000

    perldoc updated

--- Ccsds/lib/Ccsds/StdTime.pm
+++ Ccsds/lib/Ccsds/StdTime.pm
@@ -44,8 +44,10 @@ sub CDS {
                             UBInt32('Mil'),
                             subMilliCDS($milli_size),
                     );
-    } 
-    warn "Values $day_size and $milli_size not allowed for CDS form of CCSDS standard";
+    } else { 
+        die "Values $day_size and $milli_size not allowed for CDS form of CCSDS standard"; 
+        return undef;
+    }
 }
 
 sub CUC {
@@ -69,18 +71,56 @@ our @EXPORT = qw(CDS CUC $p_Field);
 
 =head1 SYNOPSIS
 
-This library defines standard time structures
-In this Recommendation, four Levels of time code formats can be defined based on the four degrees of interpretability of the code.
+    BEGIN {
+        use Ccsds::StdTime;
+        our $Sat_Time=CUC(4,2);
+    }
 
-It allows to automatically decode common formats wether self-identified (through PField) or selecting it from the caller application
-Currently, self-identified OBT type (based on Pfield) is not done, it must be provided by the caller
-TFields decoded are:
-CUC:
-# 1 to 4 octets of Coarse time, 0 to 3 of Fine time
-# This allows 136 years and 60ns granularity
-CDS:
-# 16 or 24 bits for DAY, 32 bits for MS of the day, submilliseconds of ms (0,16 or 32 bits)
-# This code is UTC based and leap second correction has to be made
+    use Ccsds::TM::SourcePacket qw($TMSourcePacket $TMSourcePacketHeader $TMSourceSecondaryHeader);
+    $TMSourcePacket->decode($data);
+
+=head1 DESCRIPTION
+
+In the Recommendation, four Levels of time code formats can be defined based on the four degrees of interpretability of the code.
+
+This library allows to decode CUC and CDS formats.
+Currently, PField and self-identified OBT type is not auto-detected, it must be provided by the user if needed
+
+=head1 Standard structures
+
+=head3 CUC
+
+1 to 4 octets of Coarse time, 0 to 3 of Fine time
+This allows 136 years and 60ns granularity
+
+    #Sample customization for PROJECT
+    #################################
+
+    use Ccsds::StdTime;
+    # CUC 48 bits
+    our $Sat_Time = CUC(4,2);
+    # or CUC 56 bits
+    our $Sat_Time = CUC(4,3);
+
+=head3 CDS
+
+16 or 24 bits for DAY, 32 bits for MS of the day, submilliseconds on 0,16 or 32 bits
+This code is UTC based and leap second correction has to be made (with perl libraries)
+    
+    #Sample customization for PROJECT
+    #################################
+
+    use Ccsds::StdTime;
+    # CDS with DoE on 24 bits and no submilli
+    our $Sat_Time = CDS(24,0);
+
+    # CDS with DoE on 16 bits and submilli on 16 bits
+    our $Sat_Time = CDS(16,16);
+    my $cds = $decoded->{'Sat_Time'};
+    use DateTime;
+    my $obt = DateTime->new(year=>2000)->add(days=>$cds->{'DoE'})->add(seconds=>$cds->{'Mil'}/(65535*1000));
+    print "OBT: ", $obt . "." . $cds->{'Mic'} ,"\n";
+
 
 =head1 AUTHOR
 
@@ -91,8 +131,6 @@ Laurent KISLAIRE, C<< <teebeenator at gmail.com> >>
 Please report any bugs or feature requests to C<bug-data-parsebinary-network-ccsds at rt.cpan.org>, or through
 the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Data-ParseBinary-Network-Ccsds>.  I will be notified, and then you'll
 automatically be notified of progress on your bug as I make changes.
-
-
 
 
 =head1 SUPPORT

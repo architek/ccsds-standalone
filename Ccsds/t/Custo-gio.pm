703f8df263fa196b9f0724fac112ce73d2720d1f
Laurent Kislaire <teebeenator@gmail.com>
2012-07-30 00:43:15 +0200

    Custo test file in uppercase

---
+++ Ccsds/t/Custo-gio.pm
@@ -0,0 +1,20 @@
+#Sample customization for GIO
+#############################
+package Ccsds::Custo;
+use Ccsds::StdTime;
+use Data::ParseBinary;
+
+our $TMSourceSecondaryHeader = Struct('TMSourceSecondaryHeader',  ### 10 bytes
+  Value('Length',10),
+  BitStruct('SecHeadFirstField',                                  #1 byte
+    BitField('Spare1',1),
+    BitField('PUS Version Number',3),
+    Nibble('Spare2')
+  ),
+  UBInt8('Service Type'),                                         #1 byte
+  UBInt8('Service Subtype'),                                      #1 byte
+  UBInt8('Destination Id'),                                       #1 byte
+  CUC(4,2),                                                       #6 bytes
+);
+
+1;

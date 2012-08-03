f14ebdeb5eb1df42c2ef964f5b3b6221972c9262
Laurent Kislaire <teebeenator@gmail.com>
2012-08-03 21:57:50 +0200

    Multithread TM Frame decoding

--- examples/tm/parallel.pl
+++ examples/tm/parallel.pl
@@ -1,6 +1,4 @@
 #!/usr/bin/perl
-# This example shows usage of multi thread frame decoding
-
 use strict;
 use warnings;
 
@@ -15,12 +13,12 @@ my ( $nf, $np ) = ( 0, 0 );
 my $id;
 my %dist_vc;
 
-my $filename = $ARGV[0] ;
-my $n_cores=Sys::CPU::cpu_count();
+my $filename = $ARGV[0];
+my $n_cores  = Sys::CPU::cpu_count();
 
 sub output_decoder {
     my ($mess) = @_;
-#    printf "t=%.2fs \t WARNING: $mess\n", $bench_time;
+    #    printf "t=%.2fs \t WARNING: $mess\n", $bench_time;
 }
 
 sub scos_tm_decode {
@@ -30,42 +28,42 @@ sub scos_tm_decode {
 
 sub extract_tmfe_record {
     my $frame = shift;
-    my $tfh = $frame->{'TM Frame Header'};
-    my $vc = $tfh->{'Virtual Channel Id'};
+    my $tfh   = $frame->{'TM Frame Header'};
+    my $vc    = $tfh->{'Virtual Channel Id'};
     $dist_vc{$id}->{$vc}++;
 }
 
 #Split number of records into n_cores
 sub conf_par {
-	my $sz= -s $filename;
-	my $slice=$sz/$Ccsds::Custo::config_tm->{record_len};
-	print "Detected $slice blocks\n";
+    my $sz    = -s $filename;
+    my $slice = $sz / $Ccsds::Custo::config_tm->{record_len};
+    print "Detected $slice blocks\n";
 
-	$slice=1 + int($slice/$n_cores);
-	
+    $slice = 1 + int( $slice / $n_cores );
+
     print "Slicing in $slice frames\n";
-	$slice;
+    $slice;
 }
 
 print "Using $n_cores cores\n";
 my $pm = new Parallel::ForkManager($n_cores);
 
-my $steps=conf_par;
-$Ccsds::Custo::config_tm->{frame_nr}=$steps;
+my $steps = conf_par;
+$Ccsds::Custo::config_tm->{frame_nr} = $steps;
 
-for (0 .. $n_cores-1) {
-    $pm->start and next; 
+for ( 0 .. $n_cores - 1 ) {
+    $pm->start and next;
     $id = $_;
     print "New thread $id\n";
-    $Ccsds::Custo::config_tm->{skip}= $_ * $steps;
+    $Ccsds::Custo::config_tm->{skip} = $_ * $steps;
     $nf = Ccsds::TM::File::read_frames( $filename, $Ccsds::Custo::config_tm );
-    
-    print "-"x80 , "\n", "Thread $id, from frame ", 
-          $Ccsds::Custo::config_tm->{skip}, ", nframes=", 
-          $Ccsds::Custo::config_tm->{frame_nr} ,":\n";
-    print Dumper(\%dist_vc);
+
+    print "-" x 80, "\n", "Thread $id, from frame ",
+      $Ccsds::Custo::config_tm->{skip},     ", nframes=",
+      $Ccsds::Custo::config_tm->{frame_nr}, ":\n";
+    print Dumper( \%dist_vc );
     print "$np non_idle packets\n";
 
-    $pm->finish; # Terminates the child process
+    $pm->finish;    # Terminates the child process
 }
 $pm->wait_all_children;
